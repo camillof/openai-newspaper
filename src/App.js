afdc956713562a0b71227a15a9be17497f634d73
Camillo Facello <52419977+camillof@users.noreply.github.com>
2023-04-21 12:34:56 -0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import CoverGenerator from './components/CoverGenerator/CoverGenerator';
 import './styles.scss';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Landing/>} />
-        <Route path='/view' element={<CoverGenerator/>} />
-      </Routes>
+      <div className='content'>
+        <Routes>
+          <Route exact path='/' element={<Landing/>} />
+          <Route path='/view' element={<CoverGenerator/>} />
+        </Routes>
+      </div>
+      <Footer/>
     </BrowserRouter>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>Powered by OpenAI API</p>
+      <p>View on GitHub</p>
+    </footer>
+  );
+};
+
+export default Footer;
